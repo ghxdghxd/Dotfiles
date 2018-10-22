5ab8a24725b91654e0c96888de7dc749d16be933
ghxdghxd <guojt-4451@163.com>
2018-10-22 21:37:39 +0800

    remove sublime-text

--- autorun.sh
+++ autorun.sh
@@ -16,8 +16,8 @@ sshfs -o transform_symlinks -o reconnect -o follow_symlinks -o uid=1000 -o gid=1
 a:/home/`grep -A2 "Host l" ~/.ssh/config|sed 1,2d|cut -d ' ' -f 2` ~/service/hpc
 fi
 
-ping -c1 -W1 `grep -A1 "Host p" ~/.ssh/config|sed 1d|cut -d ' ' -f 2` &> /dev/null
-if [ "$?" = "0" ]; then
-sshfs -o transform_symlinks -o reconnect -o follow_symlinks -o uid=1000 -o gid=1000 -o umask=022 \
-p:/media/DTSE9 ~/MyDatabase
-fi
+# ping -c1 -W1 `grep -A1 "Host p" ~/.ssh/config|sed 1d|cut -d ' ' -f 2` &> /dev/null
+# if [ "$?" = "0" ]; then
+# sshfs -o transform_symlinks -o reconnect -o follow_symlinks -o uid=1000 -o gid=1000 -o umask=022 \
+# p:/media/DTSE9 ~/MyDatabase
+# fi
