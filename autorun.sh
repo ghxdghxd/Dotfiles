cec8dcd86572104e1c7ee9e1957b40cd27835286
ghxdghxd <guojt-4451@163.com>
2020-07-11 11:28:24 +0800

    tmux,autorun

--- autorun.sh
+++ autorun.sh
@@ -7,7 +7,7 @@ set -o nounset                              # Treat unset variables as an error
 ping -c1 -W1 `grep -A1 "Host l" ~/.ssh/config|sed 1d|cut -d ' ' -f 2` &> /dev/null
 if [ "$?" = "0" ]; then
 sshfs -o transform_symlinks -o reconnect -o follow_symlinks -o uid=1000 -o gid=1000 -o umask=022 \
-l:/home/`grep -A2 "Host l" ~/.ssh/config|sed 1,2d|cut -d ' ' -f 2` ~/service/gate
+l:/public/home/`grep -A2 "Host l" ~/.ssh/config|sed 1,2d|cut -d ' ' -f 2` ~/service/gate
 fi
 
 ping -c1 -W1 `grep -A1 "Host a" ~/.ssh/config|sed 1d|cut -d ' ' -f 2` &> /dev/null
@@ -21,3 +21,7 @@ fi
 # sshfs -o transform_symlinks -o reconnect -o follow_symlinks -o uid=1000 -o gid=1000 -o umask=022 \
 # p:/media/DTSE9 ~/MyDatabase
 # fi
+
+# guake
+
+#guake
