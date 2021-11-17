9fe15137abcce59d4980fd28e878ab5a6ffb7752
guojt-4451@163.com <guojt-4451@163.com>
2021-11-17 13:30:09 +0800

    ignore

--- autorun.sh
+++ autorun.sh
@@ -4,11 +4,11 @@ set -o nounset                              # Treat unset variables as an error
 
 #sshfs
 
-ping -c1 -W1 `grep -A1 "Host l" ~/.ssh/config|sed 1d|cut -d ' ' -f 2` &> /dev/null
-if [ "$?" = "0" ]; then
+#ping -c1 -W1 `grep -A1 "Host l" ~/.ssh/config|sed 1d|cut -d ' ' -f 2` &> /dev/null
+#if [ "$?" = "0" ]; then
 sshfs -o transform_symlinks -o reconnect -o follow_symlinks -o uid=1000 -o gid=1000 -o umask=022 \
 l:/public/home/`grep -A2 "Host l" ~/.ssh/config|sed 1,2d|cut -d ' ' -f 2` ~/service/gate
-fi
+#fi
 
 ping -c1 -W1 `grep -A1 "Host a" ~/.ssh/config|sed 1d|cut -d ' ' -f 2` &> /dev/null
 if [ "$?" = "0" ]; then
@@ -25,3 +25,8 @@ fi
 # guake
 
 #guake
+
+
+# sudo mount /dev/sda2 ~/data
+#mount /dev/sda2 ~/data
+
